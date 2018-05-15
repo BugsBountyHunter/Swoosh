d5d508744717d7e0d06a52923f0f7f2b8ae6de84
Admin <Ahmed@admins-mac.local>
2018-05-16 02:15:46 +0300

    First layer dev ... update log , title , tag , and back ground

---
+++ View/BorderButton.swift
@@ -0,0 +1,25 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Ahmed on 9/1/1439 AH.
+//  Copyright © 1439 AH Steph_saber. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
